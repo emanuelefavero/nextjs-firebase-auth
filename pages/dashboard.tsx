1c19349bc62944a3a3bd47452b7d3fd97791815e
emanuelefavero <emafaux89@gmail.com>
2023-01-31 04:23:58 +0100

    add custom title to dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,20 @@
 import { Container } from 'react-bootstrap'
+import Head from 'next/head'
 import { useAuth } from '@/context/AuthContext'
 
 const Dashboard = () => {
   const { user } = useAuth()
 
   return (
-    <Container className='mt-3'>
-      <h1>{user && user.email} secret dashboard</h1>
-      <p>This route is protected</p>
-    </Container>
+    <>
+      <Head>
+        <title>{user ? `${user} dashboard` : 'Next.js Firebase Auth'}</title>
+      </Head>
+      <Container className='mt-3'>
+        <h1>{user && user.email} secret dashboard</h1>
+        <p>This route is protected</p>
+      </Container>
+    </>
   )
 }
 
